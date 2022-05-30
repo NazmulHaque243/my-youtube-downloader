2c95ea991f296a8a7163bba34477f88c42e36891
NazmulHaque243 <90323176+NazmulHaque243@users.noreply.github.com>
2022-05-29 18:05:19 -0700

    Update youtube_downloader_using_pytube.py

--- youtube_downloader_using_pytube.py
+++ youtube_downloader_using_pytube.py
@@ -1,5 +1,33 @@
 from pytube import YouTube, streams
-from pytube.cli import on_progress
+from tqdm import tqdm
+import os
+banner="""
+__   __         _____      _
+\ \ / /__  _   |_   _|   _| |__   ___
+ \ V / _ \| | | || || | | | '_ \ / _ \
+  | | (_) | |_| || || |_| | |_) |  __/
+  |_|\___/ \__,_||_| \__,_|_.__/ \___|
+
+ ____                      _                 _           _ _
+|  _ \  _____      ___ __ | | ___   __ _  __| | ___ _ __| | |
+| | | |/ _ \ \ /\ / / '_ \| |/ _ \ / _` |/ _` |/ _ \ '__| | |
+| |_| | (_) \ V  V /| | | | | (_) | (_| | (_| |  __/ |  |_|_|
+|____/ \___/ \_/\_/ |_| |_|_|\___/ \__,_|\__,_|\___|_|  (_|_)
+
+"""
+# from pytube.cli import on_progress
+
+
+def on_progress(stream,chunk,bytes_remaining):
+    total=stream.filesize
+    for data in tqdm(iterable=chunk,total=total,unit="kb"):
+        pass
+
+    
+def on_complate(stream,file_path):
+    print("download complate")
+
+
 
 
 
@@ -10,22 +38,36 @@ def youtube_downloader(links_of):
     # youtube_link=links_of[urls]
     yt=YouTube(links_of,on_progress_callback=on_progress)
     print("please wait.....")
-    print(yt.title)
-    print("wait for information...")
+    # print("video clips")
+    print(f"Title: {yt.title}")
+    print("wait for more information...")
     for index,i in enumerate(yt.streams):
         if i.type=="video":
-            print("video clips number")
-            print(f"{index}: {i.resolution}{i.filesize//1024}kb")
+            print("---------------------video-------------------")
+            print(f"{index}: {i.resolution}{round(i.filesize/(1024*1024),2)}MB")
         else:
-            print("audio clips number")
-            print(f"{index}: {i.abr}{i.filesize}kb")
+            print("---------------------audio-------------------")
+            print(f"{index}: {i.abr}{round(i.filesize/(1024*1024),2)}MB")
 
             
     inpu=int(input("Enter number for download:"))
     s=yt.streams[inpu]
-    s.download()
+    dirs=os.getcwd()+r"\ntube"
+    if not dirs:
+        os.mkdir(dirs)
 
-# driver.close()
+    s.download(dirs)
+
+
+
+
+
 if __name__=="__main__":
-    urls=(input("enter a yotube video link:"))
-    youtube_downloader(urls)
+    while True:
+        print(banner)
+        print("ready to download video from youtube enter link or enter exit to terminate")
+        urls=(input("enter a yotube video link:"))
+        if urls=="exit":
+            exit()
+        youtube_downloader(urls)
+        
